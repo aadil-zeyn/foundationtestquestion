7d8759c2982ef403741e17dd41fef2c9b22af4ec
Aadil <aadhu1857@gmail.com>
2023-05-08 12:32:29 +0530

    first commit

--- src/main/java/com/example/test/controller/Librarycontroller.java
+++ src/main/java/com/example/test/controller/Librarycontroller.java
@@ -21,65 +21,35 @@ public class Librarycontroller {
     private UserRepository userRepository;
     @PostMapping("/addbook")
     public ResponseEntity<Book> create(@RequestBody Book book){
-        bookRepository.save(book);
-        return ResponseEntity.ok().body(book);
+
+        return ResponseEntity.ok().body(null);
     }
     @PostMapping("/adduser")
     public ResponseEntity<User> createUser(@RequestBody User user){
-        userRepository.save(user);
-        return ResponseEntity.ok().body(user);
+
+        return ResponseEntity.ok().body(null);
     }
     @GetMapping("/book/{id}")
     public ResponseEntity<Book> getBook(@PathVariable Long id){
-        Optional<Book> book=bookRepository.findById(id);
-        if(book.isPresent()){
-            return ResponseEntity.ok().body(bookRepository.findById(id).orElse(null));
-        }
-        else{
-            return ResponseEntity.noContent().build();
-        }
+        return ResponseEntity.ok().body(null);
+
+
     }
     @GetMapping("/user/{id}")
     public ResponseEntity<User> getUser(@PathVariable Long id){
-//        Optional<User> user=userRepository.findById(id);
-//        if(user.isPresent()){
-//            return ResponseEntity.ok().body(userRepository.findById(id).orElse(null));
-//        }
-//        else{
-//            return ResponseEntity.noContent().build();
-//        }
-        User user=userRepository.findById(id).orElse(null);
-        if(user==null){
-            return ResponseEntity.noContent().build();
-        }
-        return ResponseEntity.ok().body(user);
+
+        return ResponseEntity.ok().body(null);
     }
 
     @PutMapping("/update/{id}")
     public ResponseEntity<Book> updateDetails(@RequestBody Book book, @PathVariable long id){
-        Book buk = null;
-        Optional<Book> updatebook = bookRepository.findById(id);
-        if(updatebook.isPresent()){
-            buk = updatebook.get();
-            buk.setId(id);
-            buk.setName(book.getName());
-            bookRepository.save(buk);
-            return ResponseEntity.ok().body(buk);
-        }
-        else{
-            return ResponseEntity.noContent().build();
-        }}
+
+            return ResponseEntity.ok().body(null);
+       }
 
         @DeleteMapping("/delete/{id}")
         public ResponseEntity<Boolean> deleteBook(@PathVariable long id){
-            Optional<Book> buk = bookRepository.findById(id);
-            if(buk.isEmpty()){
-                return ResponseEntity.ok(false);
-            }
-            else{
-                bookRepository.deleteById(id);
-                return ResponseEntity.ok(true);
-            }
-        }
-    }
+          return ResponseEntity.ok(null);
+
+    }}
 
--- src/main/java/com/example/test/repository/BookRepository.java
+++ src/main/java/com/example/test/repository/BookRepository.java
@@ -4,6 +4,6 @@ import com.example.test.model.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
-public interface BookRepository extends JpaRepository<Book,Long> {
+
+public interface BookRepository {
 }
